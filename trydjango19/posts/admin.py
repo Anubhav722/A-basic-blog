4a352823c642c6cefa2e29c71de179ab5564f9ba
Anubhav Singh <anubhavs286@gmail.com>
2016-11-17 18:46:23 +0530

    Update admin.py

--- trydjango19/posts/admin.py
+++ trydjango19/posts/admin.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-class PostAdmin(admin.ModelAdmin):
-     list_display=['title','updated', 'timestamp']
-     list_display_links=['updated']
-     list_filter=['updated', 'timestamp']
-     list_editable=['title']
-     search_fields=['title', 'content']
+class PostAdmin(admin.ModelAdmin): # ModelAdmin is used for customizing the admin
+     list_display=['title','updated', 'timestamp'] # list_display -displays the arrays in the admin page
+     list_display_links=['updated'] # list_display_links -it will make the text in the updated array to be functionable
+     list_filter=['updated', 'timestamp'] # list_filter - it adds the filtering options on the right side of the admin page
+     list_editable=['title'] # list_editable - let's the user edit the title without actually entering the link
+     search_fields=['title', 'content'] # search_fields - will look thru the title and content for the string entered by the user
      class Meta:
          models=Post
 
